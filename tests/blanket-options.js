ce927ab03e3984b30bb52015f99745248e0fb6d6
davidgoli <davigoli@gmail.com>
2015-12-08 18:31:55 -0800

    add blanket code coverage

---
+++ tests/blanket-options.js
@@ -0,0 +1,18 @@
+/* globals blanket, module */
+
+var options = {
+  modulePrefix: 'ember-cli-pull-to-refresh',
+  filter: '//.*ember-cli-pull-to-refresh/.*/',
+  antifilter: '//.*(tests|template).*/',
+  loaderExclusions: [],
+  enableCoverage: true,
+  cliOptions: {
+    reporters: ['json'],
+    autostart: true
+  }
+};
+if (typeof exports === 'undefined') {
+  blanket.options(options);
+} else {
+  module.exports = options;
+}
