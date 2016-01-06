99e526ed43cbdbad5c95036f352c5798aa13fc16
davidgoli <davigoli@gmail.com>
2016-01-05 18:24:33 -0800

    add Function.prototype.bind polyfill for phantomjs 1.9

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -3,4 +3,30 @@ import {
   setResolver
 } from 'ember-qunit';
 
+(function fnBindPolyfill() {
+  if (typeof Function.prototype.bind === 'function') {
+    return;
+  }
+
+  Function.prototype.bind = function (oThis) {
+    if (typeof this !== "function") {
+      throw new TypeError(
+        "Function.prototype.bind - what is trying to be bound is not callable"
+      );
+    }
+
+    let aArgs = Array.prototype.slice.call(arguments, 1);
+    let fToBind = this;
+    let FNOP = function () {};
+    let fBound = function () {
+      let thisObj = (this instanceof FNOP && oThis) ? this : oThis;
+      let args = aArgs.concat(Array.prototype.slice.call(arguments));
+      fToBind.apply(thisObj, args);
+    };
+
+    FNOP.prototype = this.prototype;
+    fBound.prototype = new FNOP();
+  };
+})();
+
 setResolver(resolver);
